ba47a06bfc1270c64d9517737b17658a58a06dd4
Exoutia <jhav365@gmail.com>
2023-02-08 14:44:33 +0530

    adding the rich module and different things from it

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from random import choice
 import pathlib
 from string import ascii_letters
+from rich.console import Console
+from rich.theme import Theme
+console = Console(width=40, theme=Theme({'warning': 'red on yellow'}))
 
 def get_random_word(word_list: list, num: int = 5):
     """Get a random five leter word (default 5 you can change it using num key)
@@ -50,27 +53,51 @@ def show_guess(guess_word: str, correct_word: str):
     print(f'Wrong letters: {", ".join(sorted(wrong_letters))}')
 
 
+def styled_show_guess(guesses, word):
+    for guess in guesses:
+        styled_guess = []
+        for letter, correct in zip(guess, word):
+            if letter == correct:
+                style = 'bold white on green'
+            elif letter in word:
+                style = 'bold white on yellow'
+            elif letter in ascii_letters:
+                style = 'white on #666666'
+            else:
+                style = 'dim'
+            styled_guess.append(f'[{style}]{letter}[/]')
+        console.print(''.join(styled_guess), justify='center')
+
 def game_over(word):
     print(f'The word was {word}')
+
+
+def refresh_page(headline):
+    console.clear()
+    console.rule(f'[bold blue]:leafy_green: {headline} :leafy_green:[/]')
+
 
 def main():
     # pre-process
     words_path = pathlib.Path(__file__).parent/'wordList.txt'
     word_list = words_path.read_text(encoding='utf-8').split('\n')
     word = get_random_word(word_list)
+    guesses = ["_"*5]*6
 
     # Process (main loop)
-    for guess_num in range(1, 7):
-        guess = input(f'\nGuess {guess_num}: ').upper()
+    for idx in range(6):
+        refresh_page(f"Guess {idx + 1}")
+        styled_show_guess(guesses, word)
 
-        show_guess(guess, word)
-        if guess == word:
+        guesses[idx] = input('\nGuess word: ').upper()
+        if guesses[idx] == word:
             print("This is the correct word bro.")
+            refresh_page(f"Guess {idx + 1}")
+            styled_show_guess(guesses, word)
             break
 
     # Post-process
-    else:
-        game_over(word)
+    game_over(guesses, word)
 
 if __name__ == "__main__":
     main()
